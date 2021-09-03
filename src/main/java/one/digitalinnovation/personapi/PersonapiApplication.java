e254b89cddf86ff0dd7f942b919fbd5d04aeb303
Emerson Costa <emerson.costa2785@gmail.com>
2021-09-03 20:09:18 -0300

    Projeto Refatorado

--- src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
+++ src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -1,4 +1,4 @@
-package one.digitalinnovation.personapi.service;
+package one.digitalinnovation.personapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PersonapiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PersonapiApplication.class, args);
 	}
 
